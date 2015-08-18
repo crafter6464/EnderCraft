2549209397d6ba935b417debcd680b4f8e00a33f
kyle26755@gmail.com <kyle26755@gmail.com>
2015-08-18 18:55:07 -0400

    Added GuiHandler

--- src/main/java/com/crafter6464/endercraft/init/EnderCraftItems.java
+++ src/main/java/com/crafter6464/endercraft/init/EnderCraftItems.java
@@ -3,8 +3,6 @@ package com.crafter6464.endercraft.init;
 import com.crafter6464.endercraft.item.ItemEC;
 
 public class EnderCraftItems {
-public static ItemEC test;
-public static ItemEC test2;
 	public static void init() {
 		
 	}
